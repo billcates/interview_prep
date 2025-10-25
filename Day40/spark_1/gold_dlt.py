d9e15c2b8da0f73ab59e9f5a5289d276436c2f1d
billcates <56115142+billcates@users.noreply.github.com>
2025-10-25 08:51:13 +0530

    Day40:spark:gold_dlt

---
+++ Day40/spark_1/gold_dlt.py
@@ -0,0 +1,15 @@
+from pyspark import pipelines as dp
+from pyspark.sql.functions import current_timestamp,col,sum
+
+
+@dp.table(
+        name="sales_gold",
+        comment="Gold Table"
+)
+def load_gold_table():
+    df = dp.read_stream("sales_silver").withWatermark("sale_date","1 day")
+    df=(df.groupBy("category","sale_date").agg(
+        sum(col("total_amount")).alias("total_sale_amount"),
+        sum(col("quantity")).alias("total_quantity"))
+        .withColumn("last_updated_time",current_timestamp()))
+    return df
