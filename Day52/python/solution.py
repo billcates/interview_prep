981c7990569827113947fdc2aecc9709e0865a76
billcates <56115142+billcates@users.noreply.github.com>
2025-11-05 21:42:38 +0530

    Day52:python:bucket sort approach

--- Day52/python/solution.py
+++ Day52/python/solution.py
@@ -1,7 +1,36 @@
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # builder the counter
         ct=Counter(nums)
+        #sort based on freq
         st=sorted(ct.items(),key=lambda x:x[1],reverse=True)
+        #keep first k elements
         st=[v[0] for k,v in enumerate(st[:k])]
         return st
+    
+
+# 2nd approach - bucket sort
+
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #counter
+        ct=Counter(nums)
+        #buckets of length nums to track frequency
+        freq=[[] for i in range(len(nums)+1)]
+
+        #track frequency
+        for key,v in ct.items():
+            freq[v].append(key)
+
+        res=[]
+        # iterate the frequency from reverse till length of res is k
+        for i in range(len(freq)-1,0,-1):
+            for j in freq[i]:   
+                res.append(j)
+
+                if len(res) == k:
+                    return res
+
+
