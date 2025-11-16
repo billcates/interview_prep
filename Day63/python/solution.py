61c00b83f3766edb62decf68269f31511bbfb19a
billcates <56115142+billcates@users.noreply.github.com>
2025-11-16 13:05:03 +0530

    Day63:python:monotonic stack

--- Day63/python/solution.py
+++ Day63/python/solution.py
@@ -1,1 +1,16 @@
-#day63
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        _res=[0] * len(temperatures)
+        stack = [] #[ind,temp]
+
+        for i in range(len(temperatures)):
+
+            while stack and stack[-1][1] < temperatures[i]:
+                val=stack[-1][0]
+                _res[stack[-1][0]] = i - val
+                stack.pop()
+            
+            stack.append([i,temperatures[i]])
+        
+        return _res
+        
