d18547e149b8fd25017a8e6fe28a57bd56fe408d
billcates <56115142+billcates@users.noreply.github.com>
2025-11-18 07:40:57 +0530

    Day66:python:binary search

---
+++ Day66/python/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l=0
+        r=len(nums)-1
+               
+        while l<=r:
+            mid= l+ (r - l)//2
+            val= nums[mid]
+            print(val, l ,r)
+            
+            if val == target:
+                return mid
+            elif val > target:
+                r=mid-1
+            else:
+                l=mid+1
+        
+        return -1
+        
