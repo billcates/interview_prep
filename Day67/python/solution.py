a0938a421e91df24770cb30360f578e76c4e1329
billcates <56115142+billcates@users.noreply.github.com>
2025-11-20 21:31:13 +0530

    Day68:python:binary_search

--- Day67/python/solution.py
+++ Day67/python/solution.py
@@ -18,3 +18,19 @@ class Solution:
             else:
                 pass
         return False
+
+###### one pass simpler solutionclass Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        l, r = 0, ROWS * COLS - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            row, col = m // COLS, m % COLS
+            if target > matrix[row][col]:
+                l = m + 1
+            elif target < matrix[row][col]:
+                r = m - 1
+            else:
+                return True
+        return False
