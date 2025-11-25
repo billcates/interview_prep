78bc249624750d0e8599a3a6f944c0a7910ff6a9
billcates <56115142+billcates@users.noreply.github.com>
2025-11-25 08:39:10 +0530

    Day73:python:recursion

--- Day73/python2/solution.py
+++ Day73/python2/solution.py
@@ -18,3 +18,19 @@ class Solution:
         _res.next =list1 or list2
         
         return head.next
+    
+###################
+#recursion
+
+def mergeTwoLists_recursive(list1, list2):
+    if not list1:
+        return list2
+    if not list2:
+        return list1
+    
+    if list1.val < list2.val:
+        list1.next = mergeTwoLists_recursive(list1.next, list2)
+        return list1
+    else:
+        list2.next = mergeTwoLists_recursive(list2.next, list1)
+        return list2
