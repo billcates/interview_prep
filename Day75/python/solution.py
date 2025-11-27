6cbe71a32b5bafd4df7e9627b7753a90fadf4480
billcates <56115142+billcates@users.noreply.github.com>
2025-11-27 08:02:23 +0530

    Day75:python: linkedlist

---
+++ Day75/python/solution.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        tmp=head
+        ct=0
+        while tmp:
+            ct+=1
+            tmp=tmp.next
+        pos=ct-n
+
+
+        if pos==0:
+            return head.next
+        ct=1
+        tmp=head
+
+        while ct<=pos:
+            if ct==pos:
+                next=tmp.next
+                next_2=next.next if next else None
+                tmp.next=next_2
+                return head
+            ct+=1
+            tmp=tmp.next
+        
