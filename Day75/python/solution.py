6765295440e582fbf2284ea145bd1d97ba6362c4
billcates <56115142+billcates@users.noreply.github.com>
2025-11-27 08:15:09 +0530

    Day75:python: efficient two pointer approach

--- Day75/python/solution.py
+++ Day75/python/solution.py
@@ -27,3 +27,23 @@ class Solution:
             ct+=1
             tmp=tmp.next
         
+########################
+#two pointers
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy=ListNode(0,head)
+        left=dummy
+        right=head
+
+        while n>0:
+            right=right.next
+            n-=1
+        
+        while right:
+            left=left.next
+            right=right.next
+        
+        left.next=left.next.next
+        return dummy.next
