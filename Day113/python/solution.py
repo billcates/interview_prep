b1da728cb147ca504e78ae07678f826298d04959
billcates <56115142+billcates@users.noreply.github.com>
2026-01-04 12:36:16 +0530

    Day113:python: tabulation approach

--- Day113/python/solution.py
+++ Day113/python/solution.py
@@ -22,4 +22,19 @@ class Solution:
         res=dfs(0,0)
         return res
   
+# bottom up approach
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[[0]*n for _ in range(m)]
+
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if i==m-1 or j==n-1:
+                    dp[i][j]=1
+                else:
+                    dp[i][j]=dp[i+1][j]+dp[i][j+1]
+        
+        return dp[0][0]
+  
  
