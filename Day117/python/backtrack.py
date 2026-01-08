6b8a709589c4c43a19a83d376dd870abb5a0b436
billcates <56115142+billcates@users.noreply.github.com>
2026-01-08 07:26:49 +0530

    Day117:python: backtracking solution

---
+++ Day117/python/backtrack.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo={}
+
+        def dfs(i, curr_sum):
+            if i == len(nums):
+                return 1 if curr_sum == target else 0
+            
+            if (i,curr_sum) in memo:
+                return memo[(i,curr_sum)]
+            
+            memo[(i,curr_sum)]=(dfs(i + 1, curr_sum + nums[i]) +
+                dfs(i + 1, curr_sum - nums[i])
+            )
+            return memo[(i,curr_sum)]
+        
+        return dfs(0, 0)
