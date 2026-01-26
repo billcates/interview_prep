d0816c05ec02076d2634a83334bd94f4914fb5e7
billcates <56115142+billcates@users.noreply.github.com>
2026-01-26 10:53:51 +0530

    Day135: sql: exchange seats

---
+++ Day135/sql2/solution.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+select 
+case when id%2=0 then id -1
+     when id%2!=0 and id!=(select count(*) from seat)then id+1 
+     else id 
+end as id,
+student
+from seat
+order by id
