5ef576cdea3abe918cf5c411bd78829b76f49019
billcates <56115142+billcates@users.noreply.github.com>
2026-01-28 08:09:48 +0530

    Day137:sql:optimized

--- Day137/sql/solution.sql
+++ Day137/sql/solution.sql
@@ -16,3 +16,15 @@ select id,sum(num) as num
 from cte
 group by 1
 order by 2 desc limit 1
+
+--optimized
+
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id FROM requestaccepted
+    UNION ALL
+    SELECT accepter_id  AS id FROM requestaccepted
+) t
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
