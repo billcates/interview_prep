28cbe62f3e60432b6965efc64764f9070e0b8fe3
billcates <56115142+billcates@users.noreply.github.com>
2026-02-24 08:19:57 +0530

    Day163:python: reverse

--- Day163/python/solution.py
+++ Day163/python/solution.py
@@ -6,3 +6,23 @@ class Solution:
         k=k%len(nums)
         nums[:]=nums[-k:]+nums[:-k]
 
+# another approach
+
+class Solution:
+    def reverse(self,nums, l,r):
+        while l<r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l+=1
+            r-=1
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        l=len(nums)
+
+        self.reverse(nums,0,len(nums)-1)
+        self.reverse(nums,0,k-1)
+        self.reverse(nums,k,l-1)
+        
