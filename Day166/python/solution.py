353ebdb39a1c5f60342710b0b722e01877d0c49b
billcates <56115142+billcates@users.noreply.github.com>
2026-02-27 08:53:30 +0530

    Day166:python: h index

---
+++ Day166/python/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        l = len(citations)
+        
+        for i in range(l):
+            if citations[i] >= l - i:
+                return l - i
+                
+        return 0
