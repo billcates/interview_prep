275418b914df15b0c3570193eb176187fb4168a7
billcates <56115142+billcates@users.noreply.github.com>
2026-03-01 08:22:19 +0530

    Day168:python: roman to integer

---
+++ Day168/python/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+
+        integer=0
+
+        for i in range(len(s)):
+            # print(integer)
+            if i+1<len(s) and roman[s[i+1]]>roman[s[i]]:
+                integer -=roman[s[i]]
+            else:
+                integer +=roman[s[i]]
+        
+        return integer
