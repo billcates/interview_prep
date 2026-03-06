5a7e9cff91a10ca76df65885f653d41510d5935e
billcates <56115142+billcates@users.noreply.github.com>
2026-03-06 08:15:02 +0530

    Day173:python: optimal solution

--- Day173/python/solution.py
+++ Day173/python/solution.py
@@ -14,3 +14,23 @@ class Solution:
                 if tmp>k:
                     break
         return res
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix = 0
+        dt = {0:1}
+        count = 0
+
+        for num in nums:
+            prefix += num
+            
+            if prefix - k in dt:
+                count += dt[prefix - k]
+            
+            if prefix in dt:
+                dt[prefix]+=1
+            else:
+                dt[prefix]=1
+        return count
+                
