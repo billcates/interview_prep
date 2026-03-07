545825df9b41aaf902489222cf24be7e7629c5fe
billcates <56115142+billcates@users.noreply.github.com>
2026-03-07 12:37:10 +0530

    Day174:python: rotate matrix

---
+++ Day174/python2/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l=len(matrix)
+
+        for i in range(l//2):
+            for j in range(i,l-i-1):
+                top=matrix[i][j]
+                matrix[i][j]=matrix[l-1-j][i]
+                matrix[l-1-j][i]=matrix[l-1-i][l-1-j]
+                matrix[l-1-i][l-1-j]=matrix[j][l-1-i]
+                matrix[j][l-1-i]=top
