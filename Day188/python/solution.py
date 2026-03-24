c745daed9787f2fbff02ff9a4a78b374bd2308a0
billcates <56115142+billcates@users.noreply.github.com>
2026-03-24 07:54:19 +0530

    Day188:python : rotate ll

--- Day188/python/solution.py
+++ Day188/python/solution.py
@@ -32,3 +32,40 @@ class Solution:
         last_node.next=None
 
         return new_head
+
+# optimization
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        cur=head
+        prev=None
+        ct=1
+        while cur:
+            ct+=1
+            prev=cur
+            cur=cur.next
+        k=n%ct
+        if k==0:
+            return head
+
+        till=ct-k
+        i=1
+        tmp=head
+        while i<till:
+            i+=1
+            tmp=tmp.next
+        
+        last_node=tmp
+        new_head=tmp.next
+        prev.next=head
+        last_node.next=None
+
+        return new_head
