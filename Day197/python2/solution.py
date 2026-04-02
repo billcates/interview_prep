2ddec15abde111d7d8def307931f142d8d5ebe1a
billcates <56115142+billcates@users.noreply.github.com>
2026-04-02 08:48:14 +0530

    Day197: python : level order traversal

---
+++ Day197/python2/solution.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        res=[]
+        q=deque()
+        q.append(root)
+
+        i=0
+        while q:
+            tmp=[]
+            for _ in range(len(q)):
+                node=q.popleft()
+                tmp.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if i%2==0:
+                res.append(tmp)
+            else:
+                res.append(tmp[::-1])
+            i+=1
+        return res
+        
